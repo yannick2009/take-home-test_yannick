3357ebe58146cee937632aac1cc796b9a5e6f462
yannick2009 <yannickkouakou13@gmail.com>
2025-04-04 10:55:43 +0200

    chore: project setup

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.git
+            pkgs.nodejs
+            pkgs.yarn
+          ];
+        };
+      });
+}
